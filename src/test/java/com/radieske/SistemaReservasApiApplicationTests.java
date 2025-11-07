b442b4a7807ce04c5d12325e4410b63bb1856153
Eduardo Radieske <eduardoradieske@gmail.com>
2025-11-07 14:56:29 -0300

    Dockerfile

--- src/test/java/com/radieske/SistemaReservasApiApplicationTests.java
+++ src/test/java/com/radieske/SistemaReservasApiApplicationTests.java
@@ -3,7 +3,9 @@ package com.radieske;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.radieske.reservasapi.SistemaReservasApiApplication;
+
+@SpringBootTest(classes = SistemaReservasApiApplication.class)
 class SistemaReservasApiApplicationTests {
 
 	@Test
